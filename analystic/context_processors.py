458e0345be138ad680471ed32b3d25ee7ce3b7d1
drwrong <yuhangchaney@gmail.com>
2015-05-04 21:38:47 +0200

    fix bug

--- analystic/context_processors.py
+++ analystic/context_processors.py
@@ -4,7 +4,7 @@ from analystic.models import UserIpInfo
 def website_static(request):
     online_user = cache.get("online_user", [])
     context = {
-        "online_user": online_user,
+        "online_user": len(online_user),
         "total_number": UserIpInfo.objects.count(),
         "recent_users": UserIpInfo.objects.order_by("-id")[:10]
     }
