c47ac6ced87d9e8ea01bd81946ecff31a4974664
drwrong <yuhangchaney@gmail.com>
2015-05-04 21:34:16 +0200

    add to setting

--- analystic/middleware.py
+++ analystic/middleware.py
@@ -1,11 +1,20 @@
 from analystic.models import UserIpInfo
 from django.core.cache import cache
+from django.utils import timezone
+from datetime import timedelta
+from django.core.exceptions import ObjectDoesNotExist
+
 
 class IpAnalysticMiddleware(object):
 
     def process_request(self, request):
         ip = self.get_ip_address(request)
-        UserIpInfo.object.create(ip_address=ip)
+        try:
+            UserIpInfo.objects.filter(
+                visit_time__gte=timezone.now() - timedelta(minutes=5)
+            ).get(ip_address=ip)
+        except ObjectDoesNotExist:
+            UserIpInfo.object.create(ip_address=ip)
 
     def get_ip_address(self, request):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -17,9 +26,10 @@ class IpAnalysticMiddleware(object):
 
 
 class OnlineMemberMiddleware(object):
+
     def process_request(self, request):
         session_key = request.session.session_key
         online_user = cache.get("online_user", [])
         if session_key not in online_user:
             online_user.append(session_key)
-        cache.set("online_user",  online_user, 60*5)
+        cache.set("online_user",  online_user, 60 * 5)
